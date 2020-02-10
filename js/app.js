f0f8a07f9a5542b5b1a053f854d6bb8b3e852f9e
CountNick <meijer.nick1@gmail.com>
2020-02-10 15:07:27 +0100

    :building_construction: Impleminting routie.js
    Implementing hashrouting in app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-let searchInput = '?name=morty';
 
-const endpoint = `https://rickandmortyapi.com/api/character/${searchInput}`
+let userInput = ``;
 
-console.log(endpoint)
+const endpoint = `https://rickandmortyapi.com/api/character/`
 
+let inputFilter = document.querySelectorAll('input')[0]
 
 fetch(endpoint)
     .then(res => res.json())
@@ -11,12 +11,15 @@ fetch(endpoint)
 
 
 function renderCharacters(data){
-    console.log(data)
-    
+
     const loadButton = document.querySelectorAll('button')[0];
     const main = document.querySelectorAll('main')[0];
 
-    data.results  
+    main.textContent = ''
+
+    data.results
+    .filter(element => element.name.toLowerCase()
+    .includes(userInput.toLowerCase()))  
     .forEach(element => {
         //resource: https://www.myhowtoonline.com/how-to-create-an-h1-element-with-javascript/
         // console.log(element)
@@ -28,18 +31,23 @@ function renderCharacters(data){
 
         main.appendChild(section)
 
-    });
+    })
 
-    function liveSearch(){
+inputFilter.addEventListener('input', updateCharacters)
 
-        // let inputFilter = document.querySelectorAll('input')[0]
+function updateCharacters(event){
 
-        inputFilter.addEventListener('keyup', function(){
-            let value = this.value, i;
-        })
-        
+    inputFilter.removeEventListener('input', updateCharacters)
+    userInput =  event.target.value;
 
-    }
+    // fetch(`https://rickandmortyapi.com/api/character/?name=${userInput}`)
+    // .then(res => res.json())
+    // .then(data => renderCharacters(data))
+    renderCharacters(data)
+    // showCharacters()
+    // inputFilter.removeEventListener('input', renderCharacters)
+    //https://www.florin-pop.com/blog/2019/06/vanilla-javascript-instant-search/
+}
     
     function getNext(){
         loadButton.removeEventListener('click', getNext)
