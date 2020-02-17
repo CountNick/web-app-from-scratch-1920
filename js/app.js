fd3a6702c762ec7a98bc426247a0e259202afaf4
CountNick <meijer.nick1@gmail.com>
2020-02-17 15:09:29 +0100

    :recycle: Refactored nested then
    Refactored the promis.all to fetch all pages into a async function which uses async await

--- js/app.js
+++ js/app.js
@@ -2,60 +2,83 @@
 
 
 let userInput = ``;
-
-const endpoint = `https://rickandmortyapi.com/api/character/`
-
 let inputFilter = document.querySelectorAll('input')[0]
 const main = document.querySelectorAll('main')[0];
 
-// async function getData(){
-//     let pagesReq = 0;
+async function getData(){
+//resources used: 
+//async await: https://www.taniarascia.com/promise-all-with-async-await/
+//fetching data multiple pages from api: https://stackoverflow.com/questions/40677764/how-to-fetch-data-over-multiple-pages/40681342
+    const endpoint = `https://rickandmortyapi.com/api/character/`
+    const allData =[]
+    let pagesReq = 0;
 
-//     let processedResponses = []
+    // fetch(endpoint)
+    //     .then(res => {
+    //         if(res.ok) return res.json()
+    //         else throw new Error('Something went wrong')
+    //     })
+    //     .then(jsonData => {
+    //         const apiPromises = []
+    //         pagesReq = jsonData.info.pages
 
-//     fetch(endpoint)
-//         .then(res => res.json())
-//         .then(jsonData => {
-//             const apiPromises = []
-//             pagesReq = jsonData.info.pages
+    //         for(let i = pagesReq; i>0; i--){
+    //             apiPromises.push(fetch(endpoint + `?page=${i}`)
+    //             .then(res => res.json())
+    //             .then(allData => allData))
+    //         }
 
-//             for(let i = pagesReq; i>0; i--){
-//                 apiPromises.push(fetch(endpoint + `?page=${i}`))
+    //         return apiPromises
+    //     })
+    //     .then(apiPromises => {
+    //         Promise.all(apiPromises)
+    //         .then(allData => render(allData.reverse()))
+    //     })
+    
+    let response = await fetch(endpoint);
+
+    let json = await response.json()
+
+
+    pagesReq = json.info.pages
+    
+    
+    for(let i = pagesReq; i>0; i--){
+
+        let fetchNextPage = await fetch(endpoint + `?page=${i}`)
+    
+        let nextPageToJson = await fetchNextPage.json()
+
+        allData.push(nextPageToJson)
+
+    }
+    
+    console.log(allData)
+
+}
+
+
+getData()
+    
+
+
+// fetch(endpoint)
+//     .then(res => res.json())
+//     .then(data => {
+//         //initiate router
+//         routie({
+//             '': () => {
+//               renderAllCharacters(data);
+//             },
+//             'about': () => {
+//                 console.log('about')
+//             },
+//             'character/:name': (id) => {
+//               renderCharacterDetail(id, data)
 //             }
-            
-//             Promise.all(apiPromises)
-//                 .then(responses => {
-//                     responses.map(response => response.json()
-//                     .then(data => processedResponses.push(data))
-// )                    
-//                 });
-                
-//         })
-//         return processedResponses
-// }
-
-// getData()
-//     .then(ola => console.log(ola))
-
-console.log(mustache("Hello {{name}}!", {name: "world"}))
-
-fetch(endpoint)
-    .then(res => res.json())
-    .then(data => {
-        //initiate router
-        routie({
-            '': () => {
-              renderAllCharacters(data);
-            },
-            'about': () => {
-                console.log('about')
-            },
-            'character/:name': (id) => {
-              renderCharacterDetail(id, data)
-            }
-          });
-    })
-    .catch(err => console.error(err))
+//           });
+//     })
+//     .catch(err => console.error(err))
 
 function renderAllCharacters(data){
 
@@ -72,22 +95,13 @@ function renderAllCharacters(data){
 
         let section = document.createElement('section')
 
-        // section
-        // .insertAdjacentHTML('afterbegin',
-        // `<a href=#character/:${element.id}>
-        // <img src=${element.image}>
-        // <h1>${element.name}</h1>
-        // </a>`);
-        
-        // console.log(mustache("{{#section}} {{/section}}"))
-        let characterTemplate = mustache("<a href=#character/:{{id}}><img src={{image}}> <h1>{{name}}</h1> </a>", element)
-
-        // let output = Mustache.render("<a href=#character/:{{id}}> <img src={{image}}> <h1>{{name}}</h1> </a>", element)
-
-        section.innerHTML = characterTemplate
-
-        
-        
+        section
+        .insertAdjacentHTML('afterbegin',
+        `<a href=#character/:${element.id}>
+        <img src=${element.image}>
+        <h1>${element.name}</h1>
+        </a>`);
+               
 
         //gives each section 
         section.setAttribute('id', element.id)
@@ -141,17 +155,12 @@ function renderCharacterDetail(id, data){
 
    console.log(result)
 
-//    section.insertAdjacentHTML('afterbegin', `<img src=${result.image}> <h1>${result.name}</h1> <p>Status: ${result.status}</p>
-//    <p>Species: ${result.species}</p>
-//    <p>Gender: ${result.gender}</p>
-//    <p>Origin: ${result.origin.name}</p>
-//    <p>Lives in: ${result.location.name}</p>
-//    `)
-
-
-   let characterTemplate = mustache("<img src={{image}} <h1>{{name}}</h1> <p>Species: {{species}}</p><p>Gender: {{gender}}</p><p>Origin: {{origin.name}}</p> <p>Lives in: {{species}}</p>", result)
-
-   section.innerHTML = characterTemplate
+   section.insertAdjacentHTML('afterbegin', `<img src=${result.image}> <h1>${result.name}</h1> <p>Status: ${result.status}</p>
+   <p>Species: ${result.species}</p>
+   <p>Gender: ${result.gender}</p>
+   <p>Origin: ${result.origin.name}</p>
+   <p>Lives in: ${result.location.name}</p>
+   `)
 
 
     main.appendChild(section)
